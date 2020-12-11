785ef05b16d0021a83211c3e98edc43376a26d18
Jason Stajich <jasonstajich.phd@gmail.com>
2020-12-10 23:23:25 -0800

    go back simple cluster folder

--- scripts/build_cluster_genbank.py
+++ scripts/build_cluster_genbank.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os, re, csv, sys, argparse
+from operator import itemgetter, attrgetter
 from Bio import SeqIO
 from Bio import bgzf
 
@@ -9,10 +10,12 @@ extmap = {'phmmer.domtbl': 'PHMMER',
         'TBLASTN.tab': 'TBLASTN',
         'TFASTX.tab':  'TFASTX' }
 all = ['uge3','sph3','ega3','agd3','gtb3']
-cluster_expected = { 'CL1': ['gtb3','agd3','ega3','sph3','uge3'],
-                     'CL2': ['gtb3','sph3','uge3'] }
+cluster_expected = { 'CL1': ['gtb3','agd3','ega3','sph3','uge3'], #,'uge3'
+                     'CL2': ['gtb3','sph3','uge3'] } # ,'uge3'
 argparser.add_argument('--indir',default='search',
                     help='input directory')
+argparser.add_argument('--maxdist',default=15000,
+                        help='maximum distance between genes to still be classified a cluster')
 argparser.add_argument('--lookupname',default='jgi_names.tab',
                         help='Species names lookup TSV file')
 argparser.add_argument('--gbkdir',default='GBK',
@@ -25,6 +28,7 @@ args = argparser.parse_args()
 
 if not os.path.isdir(args.clusterout):
     os.mkdir(args.clusterout)
+
 for clname in cluster_expected:
     cldir = os.path.join(args.clusterout,clname)
     if not os.path.isdir(cldir):
@@ -63,7 +67,7 @@ for fname in os.listdir(args.indir):
             hits[species] = {type: os.path.join(args.indir,fname)}
 
 for species in hits:
-    cluster = {}
+
     record_dict = SeqIO.index(os.path.join(args.gbkdir,"{}.gbf.gz".format(species)),'genbank')
     phmmerhits = {}
     # is there an agd3 or gtb3 in the protein search?
@@ -80,58 +84,82 @@ for species in hits:
         incsv = csv.reader(fh, delimiter="\t")
         seen = {}
         ctgrange = {}
+        rows = []
         for row in incsv:
-            q = row[0]
-            h = row[1]
-            if q not in phmmerhits:
-                continue
-            hstart = min(int(row[8]),int(row[9]))
-            hend   = max(int(row[8]),int(row[9]))
+            row[8] = int(row[8])
+            row[9] = int(row[9])
+            rows.append(row)
 
-            if h not in cluster:
-                cluster[h] = []
+    last_end = None
+    last_ctg = None
+    cluster = []
+    for row in sorted(rows,key=itemgetter(1,8)):
+        q = row[0]
+        h = row[1]
+        if q not in phmmerhits:
+            continue
+        hstart = min(int(row[8]),int(row[9]))
+        hend   = max(int(row[8]),int(row[9]))
 
-            cluster[h].append([q,int(row[8]),int(row[9])])
+        if not last_ctg or last_ctg != h:
+            print("new cluster for {} {} {} {}".format(h,hstart,hend,q))
+            cluster.append([h,hstart,hend,[q]])
+        elif last_end and abs(hstart - last_end) > args.maxdist:
+            print("new cluster [too far] for {} {} {} {}".format(h,hstart,hend,q))
+            cluster.append([h,hstart,hend,[q]])
+        else:
+            cluster[-1][1] = min(cluster[-1][1],hstart)
+            cluster[-1][2] = max(cluster[-1][2],hend)
+            cluster[-1][3].append(q)
 
-            if h not in seen:
-                seen[h]     = {q: 1}
-                ctgrange[h] = [hstart,hend]
-            else:
-                seen[h][q]     = 1
-                ctgrange[h][0] = min(hstart,ctgrange[h][0])
-                ctgrange[h][1] = max(hend,ctgrange[h][1])
+        last_end   = hend + 1
+        last_ctg   = h
 
-        for contig in seen:
-            cluster_classify = None
-            for clname,genenames in cluster_expected.items():
-                expected_len = len(genenames)
-                gene_count   = 0
-                for g in genenames:
-                    if g in seen[contig]:
-                        gene_count += 1
-                if  gene_count == expected_len:
-                    cluster_classify = clname
-                    break
+    n = 0
+    for cl in cluster:
+        cluster_classify = None
+        ctgname = cl[0]
 
-            if not cluster_classify:
-                print("no cluster collection of genes found in {}".format(contig))
-                continue
+        for clname,genenames in cluster_expected.items():
+            expected_len = len(genenames)
+            gene_count   = []
+            print("seen names is {}".format(cl[3]))
+            for g in genenames:
+                if g in cl[3]:
+                    gene_count.append(g)
+            if len(set(gene_count)) == expected_len:
+                cluster_classify = clname
+                print("gene_count is {} expected len is {}".format(gene_count,expected_len))
+                break
 
-            outdir = os.path.join(args.clusterout,cluster_classify)
-            seq = record_dict[contig]
+        if not cluster_classify:
+            print("--> no cluster on {}:{}".format(species,ctgname))
+            continue
 
-            print("seq is {}".format(seq.id))
-            print("slicing for {}_{}:{}..{}".format(species,contig,ctgrange[contig][0],ctgrange[contig][1]))
-            slice = seq[ ctgrange[ contig][0]:ctgrange[contig][1] ] # cut a slice out
-            slice.id = "{}_{}".format(species,seq.id)
-            SeqIO.write(slice, os.path.join(args.clusterout,cluster_classify,"{}_{}.gbk".format(species,contig)), "genbank")
+        outdir = os.path.join(args.clusterout,cluster_classify)
+        if ctgname not in record_dict:
+            print("cannot find {} in record for {}".format(ctgname,species))
+        seq = record_dict[ctgname]
+        ctgname = cl[0]
+        left    = cl[1]
+        right   = cl[2]
+        # space out a bit more
+        left -= int(args.maxdist / 2)
+        if left < 1:
+            left = 1
+        right += int(args.maxdist / 2)
+        if right > len(seq):
+            right = len(seq)
 
-#            if 'agd3' in seen[contig] and 'gtb3' in seen[contig]:
-#
-#            if 'gtb3' in seen[contig] and len(seen[contig].keys()) > 1:
-#                seq = record_dict[contig]
-#                print("seq is {}".format(seq.id))
-#                print("slicing for {}_{}:{}..{}".format(species,contig,ctgrange[contig][0],ctgrange[contig][1]))
-#                slice = seq[ ctgrange[contig][0]:ctgrange[contig][1] ] # cut a slice out
-#                slice.id = "{}_{}".format(species,seq.id)
-#                SeqIO.write(slice, os.path.join(args.clusterout,"{}_{}.gbk".format(species,contig)), "genbank")
+        print("slicing {}_{}:{}..{}".format(species,ctgname,left,right))
+        slice = seq[ left:right ] # cut a slice out
+        slice.id = "{}_{}".format(species,seq.id)
+#        for feature in slice.features:
+#            if "gene" in feature.qualifiers:
+#                lt =  features.qualifiers.get("locus_tag")
+#                gene = feature.qualifiers.get("gene")
+#                feature.qualifiers["locus_tag"] = gene
+#                feature.qualifiers["gene"] = lt
+        SeqIO.write(slice, os.path.join(args.clusterout,
+            cluster_classify,"{}.{}_cls{}.gbk".format(species,ctgname,n)), "genbank")
+        n += 1
