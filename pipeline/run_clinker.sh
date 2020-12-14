e2dfb7789c0664a49d718b79db19725ef7b0b163
Jason Stajich <jasonstajich.phd@gmail.com>
2020-12-13 22:00:59 -0800

    add cluster renaming step

--- pipeline/run_clinker.sh
+++ pipeline/run_clinker.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/bash
-#SBATCH -p intel --mem 64gb --out logs/clinker.log -N 1 -n 8 
+#SBATCH -p intel --mem 64gb --out logs/clinker.log -N 1 -n 5
 
 module unload miniconda2
 module load miniconda3
 conda activate clinker
+module load parallel
+which perl
+PREF=1kfg
+TOP=cluster_by_subphyla
+# for CL in CL1 CL2
+# do
+#   echo "$CL"
+#   for d in $(ls $TOP/$CL | grep -v \.gbk)
+#   do
+#     if [ -d $TOP/$CL/$d ]; then
+#       echo "$CL/$d"
+#       clinker -p $TOP/$CL/$d/*.gbk -p ${PREF}_${CL}_${d}.html
+#     fi
+#   done
+# done
 
-clinker -p cluster_contigs/CL1/* -p 1kfg_CL1_GAG.html
-clinker -p cluster_contigs/CL2/* -p 1kfg_CL2_GAG.html
-
+for CL in CL1_CL2 CL1 CL2
+do
+  parallel -j 5 clinker -p $TOP/$CL/{}/*.gbk -p ${PREF}_${CL}_{}.html ::: $(ls $TOP/$CL | grep -v \.gbk)
+done
--- pipeline/run_parsing.sh
+++ pipeline/run_parsing.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/bash
-for n in $(tail -n +2 jgi_names.tab | cut -f1); do sbatch -p short -N 1 -n 1 --mem 2gb --wrap "./scripts/build_cluster_genbank.py  --query $n"; done
+#SBATCH -p short -N 1 -n 48 --mem 96gb --out logs/parsing.log
+CPU=48
+module unload perl
+module load parallel
+module load fasta
+
+parallel -j $CPU ./scripts/build_cluster_genbank.py  --query {} ::: $(tail -n +2 jgi_names.tab | cut -f1)
+
+parallel -j $CPU ./scripts/update_gbk_genes.py --query ::: $(ls cluster_contigs/CL[12]/*.gbk) 
+
+mkdir -p cluster_by_subphyla/CL1 cluster_by_subphyla/CL2
+./scripts/organize_by_clade.py cluster_contigs/CL1 cluster_by_subphyla/CL1
+./scripts/organize_by_clade.py cluster_contigs/CL2 cluster_by_subphyla/CL2
